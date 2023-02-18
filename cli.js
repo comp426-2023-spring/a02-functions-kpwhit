fbe680c1edd9f9454b976621e38ab9dd179d0b86
Koby Whitaker <kpwhit714@gmail.com>
2023-02-18 09:32:40 -0500

    Update lat and long to always be defined as number

--- cli.js
+++ cli.js
@@ -10,7 +10,7 @@ async function main() {
     if (args.h) {
         console.log("Usage: galosh.js [options] -[n|s] LATITUDE -[e|w] LONGITUDE -z TIME_ZONE");
         console.log("-h            Show this help message and exit.");
-        console.log("-n, -s        Latitude: N positive; S negative.");
+        console.log("-n, -s        Latitude: N positive; oijS negative.");
         console.log("-e, -w        Longitude: E positive; W negative.");
         console.log("-z            Time zone: uses tz.guess() from moment-timezone by default.");
         console.log("-d 0-6        Day to retrieve weather: 0 is today; defaults to 1.");
@@ -19,21 +19,26 @@ async function main() {
     }
 
     // Convert cardinal flags to lat and long
-    var latitude;
-    var longitude;
-    if (args.w) {
-        latitude = (args.w * -1).toFixed(2);
-    } else if (args.e) {
-        latitude = args.e.toFixed(2);
+    var latitude = NaN;
+    if (args.n) {
+        latitude = args.n.toFixed(2);
+    } else if (args.s) {
+        latitude = (-1 * args.s).toFixed(2);
     }
-    if (args.s) {
-        longitude = (args.s * -1).toFixed(2);
-    } else if (args.n) {
-        longitude = args.n.toFixed(2);
+
+    var longitude = NaN;
+    if (args.e) {
+        longitude = args.e.toFixed(2);
+    } else if (args.w) {
+        latitude = (-1 * args.w).toFixed(2);
     }
 
     const timezone = (args.z || moment.tz.guess());
+
+
     const response = await fetch(`https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&daily=precipitation_hours&timezone=${timezone}`);
+
+
     const data = await response.json();
 
     // Echo pretty JSON
