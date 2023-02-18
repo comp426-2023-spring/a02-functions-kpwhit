431628c4adb1e0efd6a1f65d44d8d285cedc3bea
Koby Whitaker <kpwhit714@gmail.com>
2023-02-18 09:36:03 -0500

    Fix help message

--- cli.js
+++ cli.js
@@ -10,7 +10,7 @@ async function main() {
     if (args.h) {
         console.log("Usage: galosh.js [options] -[n|s] LATITUDE -[e|w] LONGITUDE -z TIME_ZONE");
         console.log("-h            Show this help message and exit.");
-        console.log("-n, -s        Latitude: N positive; oijS negative.");
+        console.log("-n, -s        Latitude: N positive; S negative.");
         console.log("-e, -w        Longitude: E positive; W negative.");
         console.log("-z            Time zone: uses tz.guess() from moment-timezone by default.");
         console.log("-d 0-6        Day to retrieve weather: 0 is today; defaults to 1.");
